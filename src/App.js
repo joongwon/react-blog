ad3dcc9ede924d422df42f3e6e90b9328b6ba47e
joongwon <winterdawnlight@snu.ac.kr>
2023-02-06 19:19:10 +0900

    divide components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>블로그</h1>
+      <p>나만의 작은 일기장</p>
+    </header>
+  );
+}
+
+function PostList() {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
+
+function Post() {
+  return (
+    <div>
+      <article>
+        <h2>리액트 배우기</h2>
+        <p>리액트는 너무 재밌다!</p>
+      </article>
+      <button>삭제</button>
+    </div>
+  );
+}
+
+function PostCreate() {
+  return (
+    <form>
+      <input type="text" placeholder="제목을 입력하세요" />
+      <textarea placeholder="내용을 입력하세요" />
+      <button type="submit">작성</button>
+      <button type="reset">취소</button>
+    </form>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <PostCreate />
+      <PostList />
+    </main>
   );
 }
 
