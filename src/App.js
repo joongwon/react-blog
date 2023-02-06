02e7952ea7139f8680d0d0ebe5c668ad0935acfa
joongwon <winterdawnlight@snu.ac.kr>
2023-02-06 19:37:59 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -10,22 +10,26 @@ function Header() {
 }
 
 function PostList() {
+  const posts = [
+    { id: 1, title: "리액트", content: "리액트는 너무 재밌다!" },
+    { id: 2, title: "오늘의 영상", content: "NYC A-Train Sax Battle" },
+    { id: 3, title: "첫번째 글", content: "안녕하세요~~" },
+  ];
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
 
-function Post() {
+function Post({ post }) {
   return (
     <div>
       <article>
-        <h2>리액트 배우기</h2>
-        <p>리액트는 너무 재밌다!</p>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
       </article>
       <button>삭제</button>
     </div>
