e7cfea79d69c365459a8595127c7ccdb101181d0
joongwon <winterdawnlight@snu.ac.kr>
2023-02-07 20:52:51 +0900

    add top button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,24 @@
 import "./App.css";
+import { useRef } from "react";
 import { Header } from "./Header";
 import { PostListView } from "./PostListView";
 import { PostCreateForm } from "./PostCreateForm";
 
 function App() {
+  const topRef = useRef<HTMLDivElement>(null);
   return (
     <div className="App">
       <main>
+        <div ref={topRef} />
         <Header />
         <PostCreateForm />
         <PostListView />
+        <button
+          className="App__top-button"
+          onClick={() => topRef.current?.scrollIntoView({ behavior: "smooth" })}
+        >
+          맨 위로
+        </button>
       </main>
     </div>
   );
