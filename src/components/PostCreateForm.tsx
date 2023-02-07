cce90134b2f3a5d1e50dd548e127e0d57651f4c0
joongwon <winterdawnlight@snu.ac.kr>
2023-02-07 21:52:35 +0900

    add formik

--- src/components/PostCreateForm.tsx
+++ src/components/PostCreateForm.tsx
@@ -1,38 +1,29 @@
 import "./PostCreateForm.css";
 import { useState } from "react";
 import { useBlogContext } from "../contexts/BlogContext";
+import { Formik, Form, Field } from "formik";
 
 export function PostCreateForm() {
   const { createPost } = useBlogContext();
   const [isOpen, setIsOpen] = useState(false);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
   return isOpen ? (
-    <form
-      className="PostCreateForm"
-      onReset={() => setIsOpen(false)}
-      onSubmit={(e) => {
-        e.preventDefault();
+    <Formik
+      initialValues={{ title: "", content: "" }}
+      onSubmit={({ title, content }) => {
         createPost(title, content);
         setIsOpen(false);
       }}
+      onReset={() => setIsOpen(false)}
     >
-      <input
-        type="text"
-        placeholder="제목을 입력하세요"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <textarea
-        placeholder="내용을 입력하세요"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <div className="PostCreateForm__buttons">
-        <button type="submit">작성</button>
-        <button type="reset">취소</button>
-      </div>
-    </form>
+      <Form className="PostCreateForm">
+        <Field name="title" type="text" placeholder="제목을 입력하세요" />
+        <Field as="textarea" name="content" placeholder="내용을 입력하세요" />
+        <div className="PostCreateForm__buttons">
+          <button type="submit">작성</button>
+          <button type="reset">취소</button>
+        </div>
+      </Form>
+    </Formik>
   ) : (
     <button className="PostCreateForm__open" onClick={() => setIsOpen(true)}>
       글쓰기
